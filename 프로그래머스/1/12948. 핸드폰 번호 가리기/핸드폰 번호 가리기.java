34031a4793aec39397e055776574728f4254a029
NUYGNOD <askaskj3@gmail.com>
2025-04-22 23:31:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 1.81 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String phone_number) {
+        String answer = "";
+        int length = phone_number.length();
+        answer = "*".repeat(length-4)+phone_number.substring(length-4);
+        return answer;
+    }
+}
