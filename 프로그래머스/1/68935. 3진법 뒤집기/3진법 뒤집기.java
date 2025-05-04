de57f9708fad8775129eaca2a483575267453beb
NUYGNOD <askaskj3@gmail.com>
2025-05-05 08:15:13 +0900

    [level 1] Title: 3진법 뒤집기, Time: 12.10 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String a = "";
+        while(n !=0){
+            a += n%3+"";
+            n = n/3;
+        }
+        
+        return Integer.parseInt(a,3);
+    }
+}
